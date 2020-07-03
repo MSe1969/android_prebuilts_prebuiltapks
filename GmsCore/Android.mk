22356e74b2a28f425bd1ab15783e8d679dd790e1
MSe1969 <mse1969@posteo.de>
2020-07-03 21:39:51 +0200

    Update GmsCore from /e/ to e-v0.2.10.19420 incl. microG-PR #1069
    Includes https://github.com/microg/android_packages_apps_GmsCore/pull/1069
    
    Change-Id: Ib9612de405cc10e5f55661c33fbecf9a59010fb0

--- GmsCore/Android.mk
+++ GmsCore/Android.mk
@@ -11,12 +11,11 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := GmsCore
-LOCAL_SRC_FILES := GmsCore-v0.2.9.19420-5-cfc90b4.apk
+LOCAL_SRC_FILES := play-services-core-release-unsigned.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_CERTIFICATE := platform
 LOCAL_OVERRIDES_PACKAGES := com.qualcomm.location
 LOCAL_REQUIRED_MODULES := privapp-permissions-com.google.android.gms.xml
 include $(BUILD_PREBUILT)
-
