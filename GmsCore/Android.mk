11a119b64ce83b82b48083dddc34c12eca7dfab3
Philip Nagler-Frank <philip@nagler.world>
2022-03-05 13:42:31 +0100

    12L: Necessary adaptations to make LineageOS 19.1 work
    See https://github.com/lineageos4microg/android_vendor_partner_gms/pull/9
    
     * set LOCAL_PRODUCT_MODULE for all modules
     * target product partition for xml files
     * remove LOCAL_MODULE_TAGS := optional since it's the default anyways
     * GmsCore Added LOCAL_USES_LIBRARIES to Android.mk
    
    Change-Id: I0d6e2886c328ff9402bf08f9a7dba940781233a3

--- GmsCore/Android.mk
+++ GmsCore/Android.mk
@@ -2,22 +2,21 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := privapp-permissions-com.google.android.gms.xml
-LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT_ETC)/permissions
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
+LOCAL_PRODUCT_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := sysconfig-com.google.android.gms.xml
-LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/sysconfig
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT_ETC)/sysconfig
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
+LOCAL_PRODUCT_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := GmsCore
 LOCAL_SRC_FILES := play-services-core-withMapbox-withNearby-release-unsigned.apk
 LOCAL_MODULE_CLASS := APPS
@@ -25,5 +24,7 @@ LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := platform
 LOCAL_OVERRIDES_PACKAGES := com.qualcomm.location
+LOCAL_USES_LIBRARIES := com.android.location.provider
 LOCAL_REQUIRED_MODULES := privapp-permissions-com.google.android.gms.xml sysconfig-com.google.android.gms.xml
+LOCAL_PRODUCT_MODULE := true
 include $(BUILD_PREBUILT)
