7b869f384b99df39bdfd06c79caa6b98cf731787
MSe <mse1969@posteo.de>
2023-07-08 18:41:35 +0200

    GmsCore - 0.2.28.231657 - return to 'Original'
    Rework approach - use again the "original" prebuilt from microG
    With the inclusion of the Mozilla Location Services (MLS) into
    the GmsCore code, its use is only possible with a registered MLS
    API key, which is currently not available (requested one, not
    known whether and if yes, when it will be granted)
    
    Change-Id: I3aa0354b9d2f7f9b342fbfdf89c0cbbca87fafe7

--- GmsCore/Android.mk
+++ GmsCore/Android.mk
@@ -17,13 +17,23 @@ LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := microg.xml
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)
+LOCAL_SRC_FILES := microg.xml
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := GmsCore
-LOCAL_SRC_FILES := play-services-core-withMapbox-withNearby-release-unsigned.apk
+LOCAL_SRC_FILES := com.google.android.gms-231657056.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := platform
 LOCAL_OVERRIDES_PACKAGES := com.qualcomm.location
-LOCAL_REQUIRED_MODULES := privapp-permissions-com.google.android.gms.xml sysconfig-com.google.android.gms.xml
+LOCAL_REQUIRED_MODULES := privapp-permissions-com.google.android.gms.xml \
+                          sysconfig-com.google.android.gms.xml \
+                          microg.xml
 include $(BUILD_PREBUILT)
