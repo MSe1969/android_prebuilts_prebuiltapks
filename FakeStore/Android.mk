b621761c932667b004a2b30ff89af4139d6434ef
mse1969 <mse1969@posteo.de>
2024-06-15 19:39:10 +0200

    FDroid, Fakestore: Declare optional libraries
    Change-Id: Ib787f7a1abee68e18199891d6ff1d701d766a20d

--- FakeStore/Android.mk
+++ FakeStore/Android.mk
@@ -17,6 +17,9 @@ LOCAL_MODULE_CLASS := APPS
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_OPTIONAL_USES_LIBRARIES := \
+    androidx.window.extensions \
+    androidx.window.sidecar
 LOCAL_PRODUCT_MODULE := true
 LOCAL_REQUIRED_MODULES := privapp-permissions-com.android.vending.xml
 include $(BUILD_PREBUILT)
